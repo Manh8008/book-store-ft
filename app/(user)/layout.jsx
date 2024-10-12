7e13f2ed89a0c54b076c5f1d386bf2bdfcf99740
thinhhom <nguyenminhthinh858@gmail.com>
2024-10-13 01:54:05 +0700

    đang code giao diện chi tiết sản phẩm (chưa xong)

--- app/(user)/layout.jsx
+++ app/(user)/layout.jsx
@@ -22,7 +22,9 @@ export default function RootLayout({ children }) {
                     rel="stylesheet"
                 />
             </Head>
-            <body>{children}</body>
+            <body>
+                {children}
+            </body>
         </html>
     )
 }
